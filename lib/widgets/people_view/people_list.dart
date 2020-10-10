bdbada181b5d63a6ad29e12c4ca41fa9fd98cccb
Souvik Biswas <sbis1999@gmail.com>
2020-10-10 22:02:19 +0530

    Add people list

---
+++ lib/widgets/people_view/people_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_design/widgets/people_view/people_item.dart';
+
+class PeopleList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      itemCount: 30,
+      itemBuilder: (context, index) {
+        return PeopleItem();
+      },
+    );
+  }
+}
