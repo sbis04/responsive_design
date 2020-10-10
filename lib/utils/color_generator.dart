42a0ec5520b72093990c45b1bf4736cf56f61485
Souvik Biswas <sbis1999@gmail.com>
2020-10-10 12:53:47 +0530

    Add color generator

---
+++ lib/utils/color_generator.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter/material.dart';
+// import 'package:random_color/random_color.dart';
+
+// class ColorGenerator {
+//   final RandomColor _randomColor = RandomColor();
+
+//   List<Colors> _generateColor() {
+//     Color _color = _randomColor.randomColor();
+//     MyColor _myColor = getColorNameFromColor(_color);
+
+//     // final newColor = CustomColor(
+//     //   _myColor.getName,
+//     //   '#${_color.value.toRadixString(16).substring(2)}',
+//     // );
+
+//     return newColor;
+//   }
+// }
