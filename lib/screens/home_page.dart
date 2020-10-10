c46cc12c8f4b26ff092b8c0f1cf3ed5f00498f2c
Souvik Biswas <sbis1999@gmail.com>
2020-10-10 22:03:13 +0530

    Modify home screen

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_design/model/destination.dart';
 import 'package:responsive_design/res/custom_colors.dart';
 import 'package:responsive_design/screens/people_view.dart';
 
-class HomePage extends StatelessWidget {
+import 'destination_view.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +27,33 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: PeopleView(),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: CustomColors.blue_gray,
+        selectedIconTheme: IconThemeData(size: 30),
+        unselectedIconTheme: IconThemeData(size: 20),
+        selectedItemColor: CustomColors.neon_green,
+        unselectedItemColor: Colors.white60,
+        showUnselectedLabels: false,
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: allDestinations.map((Destination destination) {
+          return BottomNavigationBarItem(
+            icon: Icon(destination.icon),
+            label: destination.title,
+          );
+        }).toList(),
+      ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: allDestinations.map<Widget>((Destination destination) {
+          return DestinationView(destination);
+        }).toList(),
+      ),
+      // PeopleView(),
     );
   }
 }
