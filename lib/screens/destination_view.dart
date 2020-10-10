4bae0141e4c6bab92dac70fc8107727688f31935
Souvik Biswas <sbis1999@gmail.com>
2020-10-10 22:02:59 +0530

    Add destination view

---
+++ lib/screens/destination_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_design/model/destination.dart';
+import 'package:responsive_design/screens/people_view.dart';
+
+class DestinationView extends StatelessWidget {
+  final Destination destination;
+
+  DestinationView(this.destination);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        alignment: Alignment.center,
+        child: destination.id == 0
+            ? Container()
+            : destination.id == 1
+                ? PeopleView()
+                : Container(),
+      ),
+    );
+  }
+}
