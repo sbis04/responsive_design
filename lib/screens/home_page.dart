96a025222657f9bd2c9323fa540dc218f63821e7
Souvik Biswas <sbis1999@gmail.com>
2020-10-10 12:54:28 +0530

    Add chat view in home page

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_design/res/custom_colors.dart';
+import 'package:responsive_design/screens/chat_view.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: CustomColors.blue_gray,
+        title: Text(
+          'Flow',
+          style: TextStyle(
+            fontFamily: 'SansitaSwashed',
+            fontSize: 28,
+            letterSpacing: 1,
+          ),
+        ),
+      ),
+      body: ChatView(),
+    );
+  }
+}
