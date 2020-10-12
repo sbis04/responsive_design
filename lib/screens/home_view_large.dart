8d01bebb9ffae108770370ff4766285337c8a3eb
Souvik Biswas <sbis1999@gmail.com>
2020-10-12 20:46:27 +0530

    Add home page

---
+++ lib/screens/home_view_large.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_design/model/destination.dart';
+import 'package:responsive_design/screens/destination_view.dart';
+import 'package:responsive_design/widgets/menu_widget.dart';
+
+class HomeViewLarge extends StatefulWidget {
+  final int currentIndex;
+  final Function(int selectedIndex) onTapped;
+
+  HomeViewLarge(this.currentIndex, this.onTapped);
+
+  @override
+  _HomeViewLargeState createState() => _HomeViewLargeState();
+}
+
+class _HomeViewLargeState extends State<HomeViewLarge> {
+  int _index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.currentIndex;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Flexible(
+            flex: 2,
+            child: MenuWidget(
+              selectedIndex: _index,
+              onTapped: (selectedIndex) {
+                setState(() {
+                  _index = selectedIndex;
+                  widget.onTapped(_index);
+                });
+              },
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: IndexedStack(
+              index: _index,
+              children: allDestinations.map<Widget>((Destination destination) {
+                return DestinationView(destination);
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
