992835d9240ded8d9ad751ab4ebf49cc49e9f737
Souvik Biswas <sbis1999@gmail.com>
2020-10-12 22:30:58 +0530

    Add people list

--- lib/screens/people_view.dart
+++ lib/screens/people_view.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:responsive_design/widgets/people_view/people_list.dart';
 
 class PeopleView extends StatelessWidget {
+  final bool isNotInChatMode;
+
+  PeopleView({this.isNotInChatMode});
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: PeopleList(),
+      child: PeopleList(isNotInChatMode: isNotInChatMode),
     );
   }
 }
