a1a26cdfb52ca1fe292158d60554466d7a5ca04f
Souvik Biswas <sbis1999@gmail.com>
2020-10-14 20:20:51 +0530

    Add MediaQeuryLayout

---
+++ lib/layouts/media_query_layout.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_design/layouts/res/my_colors.dart';
+
+class MediaQueryLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    Orientation orientation = MediaQuery.of(context).orientation;
+
+    return Scaffold(
+      body: Container(
+        color: MyColors.darkGreen,
+        child: Center(
+          child: Text(
+            'View\n\n' +
+                '[MediaQuery width]: ${screenSize.width.toStringAsFixed(2)}\n\n' +
+                '[MediaQuery orientation]: $orientation',
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+        ),
+      ),
+    );
+  }
+}
